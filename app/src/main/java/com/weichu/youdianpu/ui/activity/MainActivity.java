993751b924d2aa5cda4c3965e67f1f25a421e324
tangxingchu <51697550@qq.com>
2018-05-16 18:05:45 +0800

    提交

--- app/src/main/java/com/weichu/youdianpu/ui/activity/MainActivity.java
+++ app/src/main/java/com/weichu/youdianpu/ui/activity/MainActivity.java
@@ -1,28 +1,23 @@
 package com.weichu.youdianpu.ui.activity;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.weichu.youdianpu.R;
-import com.weichu.youdianpu.ui.adapter.TabFragmentPagerAdapter;
-
-import java.util.ArrayList;
-
-import devlight.io.library.ntb.NavigationTabBar;
+import com.weichu.youdianpu.ui.fragment.HomeFragment;
+import com.weichu.youdianpu.ui.fragment.MyFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private Context mContext;
+
+    private HomeFragment mHomeFragment;
+    private MyFragment mMyFragment;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -33,144 +28,77 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initUI() {
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_horizontal_ntb);
-        viewPager.setPageTransformer(true, new ViewPager.PageTransformer() {
-            @Override
-            public void transformPage(View page, float position) {
-                final float normalizedposition = Math.abs(Math.abs(position) - 1);
-                page.setScaleX(normalizedposition / 2 + 0.5f);
-                page.setScaleY(normalizedposition / 2 + 0.5f);
-            }
-        });
-//        viewPager.setAdapter(new PagerAdapter() {
-//            @Override
-//            public int getCount() {
-//                return 5;
-//            }
-//
-//            @Override
-//            public boolean isViewFromObject(final View view, final Object object) {
-//                return view.equals(object);
-//            }
-//
-//            @Override
-//            public void destroyItem(final View container, final int position, final Object object) {
-//                ((ViewPager) container).removeView((View) object);
-//            }
-//
-//            @Override
-//            public Object instantiateItem(final ViewGroup container, final int position) {
-//                final View view = LayoutInflater.from(
-//                        getBaseContext()).inflate(R.layout.item_vp, null, false);
-//
-//                final TextView textView = view.findViewById(R.id.txt_vp_item_page);
-//                textView.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        Toast.makeText(mContext, "a", Toast.LENGTH_SHORT);
-//                    }
-//                });
-//
-//                final TextView txtPage = (TextView) view.findViewById(R.id.txt_vp_item_page);
-//                txtPage.setText(String.format("Page #%d", position));
-//
-//                container.addView(view);
-//                return view;
-//            }
-//        });
+        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
 
-        viewPager.setAdapter(new TabFragmentPagerAdapter(getSupportFragmentManager()));
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.ic_eighth, "Home"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_fifth, "Books"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_fourth, "Music"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_third, "Movies & TV"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_sixth, "Games"))
+                .setFirstSelectedPosition(0)
+                .initialise();
 
-        final String[] colors = getResources().getStringArray(R.array.default_preview);
 
-        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_horizontal);
-        final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        //getResources().getDrawable(R.drawable.ic_first),
-                        ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.ic_first),
-                        Color.parseColor(colors[0]))
-                        .selectedIcon(getResources().getDrawable(R.drawable.ic_sixth))
-                        .title("首页")
-                        .badgeTitle("NTB")
-                        .build()
-        );
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        //getResources().getDrawable(R.drawable.ic_second),
-                        ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.ic_second),
-                        Color.parseColor(colors[0]))
-//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
-                        .title("附近")
-                        .badgeTitle("with")
-                        .build()
-        );
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        //getResources().getDrawable(R.drawable.ic_third),
-                        ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.ic_third),
-                        Color.parseColor(colors[0]))
-                        //.selectedIcon(getResources().getDrawable(R.drawable.ic_seventh))
-                        .selectedIcon(ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.ic_seventh))
-                        .title("订单")
-                        .badgeTitle("state")
-                        .build()
-        );
-        models.add(
-                new NavigationTabBar.Model.Builder(
-                        //getResources().getDrawable(R.drawable.ic_fourth),
-                        ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.ic_fourth),
-                        Color.parseColor(colors[0]))
-//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
-                        .title("我的")
-                        .badgeTitle("icon")
-                        .build()
-        );
-//        models.add(
-//                new NavigationTabBar.Model.Builder(
-//                        //getResources().getDrawable(R.drawable.ic_fifth),
-//                        ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.ic_fifth),
-//                        Color.parseColor(colors[4]))
-//                        //.selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
-//                        .selectedIcon(ContextCompat.getDrawable(this.getApplicationContext(), R.drawable.ic_eighth))
-//                        .title("Medal")
-//                        .badgeTitle("777")
-//                        .build()
-//        );
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if(mHomeFragment == null) {
+            mHomeFragment = new HomeFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer, mHomeFragment);
+        }
+        fragmentTransaction.commit();
 
-        navigationTabBar.setModels(models);
-        navigationTabBar.setViewPager(viewPager, 0);
-        navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
-
-            }
+        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
 
             @Override
-            public void onPageSelected(final int position) {
-                navigationTabBar.getModels().get(position).hideBadge();
-            }
-
-            @Override
-            public void onPageScrollStateChanged(final int state) {
-
-            }
-        });
-
-        navigationTabBar.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < navigationTabBar.getModels().size(); i++) {
-                    final NavigationTabBar.Model model = navigationTabBar.getModels().get(i);
-                    navigationTabBar.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            model.showBadge();
+            public void onTabSelected(int position) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                switch (position) {
+                    case 0:
+                        if(mHomeFragment == null) {
+                            mHomeFragment = new HomeFragment();
                         }
-                    }, i * 100);
+                        fragmentTransaction.replace(R.id.fragmentContainer, mHomeFragment);
+                        break;
+                    case 1:
+                        if(mHomeFragment == null) {
+                            mHomeFragment = new HomeFragment();
+                        }
+                        fragmentTransaction.replace(R.id.fragmentContainer, mHomeFragment);
+                        break;
+                    case 2:
+                        if(mHomeFragment == null) {
+                            mHomeFragment = new HomeFragment();
+                            fragmentTransaction.replace(R.id.fragmentContainer, mHomeFragment);
+                        }
+                        break;
+                    case 3:
+                        if(mHomeFragment == null) {
+                            mHomeFragment = new HomeFragment();
+                        }
+                        fragmentTransaction.replace(R.id.fragmentContainer, mHomeFragment);
+                        break;
+                    case 4:
+                        if(mMyFragment == null) {
+                            mMyFragment = new MyFragment();
+                        }
+                        fragmentTransaction.replace(R.id.fragmentContainer, mMyFragment);
+                        break;
                 }
+                fragmentTransaction.commit();
             }
-        }, 500);
+
+            @Override
+            public void onTabUnselected(int position) {
+
+            }
+
+            @Override
+            public void onTabReselected(int position) {
+
+            }
+        });
     }
 
 }
