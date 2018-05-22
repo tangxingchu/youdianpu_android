02da050d543a1cf245bb76e848d843c4c903e894
tangxingchu <51697550@qq.com>
2018-05-22 17:51:51 +0800

    提交

--- app/src/main/java/com/weichu/youdianpu/ui/activity/MainActivity.java
+++ app/src/main/java/com/weichu/youdianpu/ui/activity/MainActivity.java
@@ -9,9 +9,15 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +28,12 @@ import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.weichu.youdianpu.R;
 import com.weichu.youdianpu.ui.fragment.HomeFragment;
 import com.weichu.youdianpu.ui.fragment.MineFragment;
@@ -51,6 +63,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Stack<Fragment> mStack = new Stack<Fragment>();
     private LocatedCity mLocatedCity;
     private FloatingActionButton mFaButton;
+    private DrawerLayout mDrawerLayout;
+
+    private IProfile profile = null;//登录用户信息
+    private Drawer result = null;//嵌套抽屉
+    private AccountHeader headerResult = null;//head头布局
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -78,6 +95,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        super.setSupportActionBar(toolbar);
+//        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawerLayout);
+        profile = getUserProfile();
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .withTranslucentStatusBar(true) //半透明效果
+                .addProfiles(profile)
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+                        switch ((int) profile.getIdentifier()) {
+                            case 101://根据不同标识符监听不同对象
+                                Toast.makeText(MainActivity.this, "头像被点击", Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                break;
+                        }
+                        return false;
+                    }
+                })
+                .withSavedInstance(savedInstanceState)
+                .build();//至此头布局head构建完成
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(headerResult)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .build();
+//        ActionBar actionBar = getSupportActionBar();
+//        if(actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(true);
+//            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+//        }
+
         mFaButton = (FloatingActionButton) findViewById(R.id.fab);
         //百度定位
         mLocationClient = new LocationClient(this);
@@ -138,6 +191,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private IProfile<ProfileDrawerItem> getUserProfile() {
+        String userLogin = getSharedPreferences("login", 0)
+                .getString("user", getResources().getString(R.string.txt_anonymous));
+
+        return new ProfileDrawerItem()
+                .withName(userLogin)
+                .withIcon(R.drawable.img_home)
+                .withIdentifier(101);
+    }
+
     private void initUI() {
         BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
 
@@ -146,7 +209,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .addItem(new BottomNavigationItem(R.drawable.ic_nearby, "附近"))
                 .addItem(new BottomNavigationItem(R.drawable.ic_shopping_cart, "购物袋"))
                 .addItem(new BottomNavigationItem(R.drawable.ic_order, "订单"))
-                .addItem(new BottomNavigationItem(R.drawable.ic_my, "我的"))
+//                .addItem(new BottomNavigationItem(R.drawable.ic_my, "我的"))
                 .setFirstSelectedPosition(mBNBPostion)
                 .setMode(BottomNavigationBar.MODE_FIXED)
                 .setBarBackgroundColor(android.R.color.white)
@@ -248,9 +311,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.title_city:
-                Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
-                break;
+//            case R.id.title_city:
+//                Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
+//                break;
         }
     }
 
@@ -260,6 +323,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .enableAnimation(true)
                 .setLocatedCity(mLocatedCity)
                 .show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main_toolbar:
+
+                break;
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return true;
     }
 
     public class LocationListener implements BDLocationListener {
@@ -279,8 +361,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Log.i("MainActivity", "街道 :" + bdLocation.getStreet());
             Log.i("MainActivity", "楼 :" + bdLocation.getBuildingName());
             String cityName = bdLocation.getCity().substring(0, bdLocation.getCity().length() - 1);
-            TextView textView = (TextView) findViewById(R.id.title_city);
-            textView.setText(cityName);
+//            TextView textView = (TextView) findViewById(R.id.title_city);
+//            textView.setText(cityName);
             mLocationClient.unRegisterLocationListener(mLocationListener);
             mLocatedCity = new LocatedCity(cityName, bdLocation.getProvince(), bdLocation.getCityCode());
         }
